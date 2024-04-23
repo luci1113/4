a40a6d219c3f1675a55f71c7d23fb534fba761eb
nirmal1311 <regsite1311@gmail.com>
2024-04-23 10:52:49 +0530

    difference add

--- pr1/client.py
+++ pr1/client.py
@@ -2,6 +2,7 @@ import xmlrpc.client
 proxy = xmlrpc.client.ServerProxy("http://localhost:8000/") #A ServerProxy instance is an object that manages communication with a remote XML-RPC server
 n=int(input("Enter the number: "))
 print("factorial of given number is : %s" % str(proxy.factorial_rpc(n)))
-
+'''RPC supports procedural programming.	RMI supports object-oriented programming.
+RPC is the older version of RMI.	While it is the successor version of RPC.'''
 
 
