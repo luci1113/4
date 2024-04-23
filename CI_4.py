59153e3a302c5aefd13c924cd12037bdaec7e172
nirmal1311 <regsite1311@gmail.com>
2024-04-23 11:43:25 +0530

    add info in CI4

--- CI_4.py
+++ CI_4.py
@@ -1,4 +1,11 @@
-# Example to Demonstrate the 
+
+#fuzzy refers to things that are not clear
+#Fuzzy Logic is a form of many-valued logic in which the truth values of variables may be any real number between 0 and 1
+'''Common Operations on fuzzy sets
+1.Union
+2. Intersection
+3.Difference
+4. Complement'''
 # Union of Two Fuzzy Sets
 A = dict()
 B = dict()
@@ -22,6 +29,7 @@ for A_key, B_key in zip(A, B):
 		
 print('Fuzzy Set Union is :', Y)
 
+#intersection
 for A_key, B_key in zip(A, B):
     A_value = A[A_key]
     B_value = B[B_key]
@@ -32,16 +40,17 @@ for A_key, B_key in zip(A, B):
         Y[B_key] = B_value
 print('Fuzzy Set Intersection is :', Y)
 print("\n")
+
+#complement 
 A = {"a": 0.2, "b": 0.3, "c": 0.6, "d": 0.6}
  
 print('The Fuzzy Set is :', A)
- 
- 
 for A_key in A:
    Y[A_key]= 1-A[A_key]
          
 print('Fuzzy Set Complement is :', Y)
 
+#Difference
 for A_key, B_key in zip(A, B):
     A_value = A[A_key]
     B_value = B[B_key]
