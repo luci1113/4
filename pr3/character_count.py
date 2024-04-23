f3ba7144192ef4172b4b675f7be6bba6763a3c05
nirmal1311 <regsite1311@gmail.com>
2024-04-23 11:17:31 +0530

    add info in CI3

--- pr3/character_count.py
+++ pr3/character_count.py
@@ -1,13 +1,20 @@
-from mrjob.job import MRJob
+from mrjob.job import MRJob #MapReduce jobs #mrjob is currently an active Framework for MapReduce programming
 import sys
 
+#With mrjob, we can write code for Mapper and Reducer in a single class. In case we don’t have Hadoop installed then also we can test the mrjob program in our local system environment
 class CharacterCount(MRJob):
+    #MapReduce is a way of writing programs designed for processing vast amounts of data
     
-    def mapper(self, _, line):
+    '''The below mapper() function defines the mapper for MapReduce and takes  
+    key value argument and generates the output in tuple format .  
+    The mapper below is splitting the line and generating a word with its own  
+    count i.e. 1'''
+    def mapper(self, _, line): 
         # Emit each character with count 1
         for char in line:
             yield char, 1
-    
+    '''The below reducer() is aggregating the result according to their key and 
+    producing the output in a key-value format with its total count'''
     def reducer(self, key, values):
         # Sum up the counts for each character
         yield key, sum(values)
