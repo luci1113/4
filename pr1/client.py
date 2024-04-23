d56df94635e0ce800b9eed49da5a6a2d4369e1eb
nirmal1311 <regsite1311@gmail.com>
2024-04-23 10:46:37 +0530

    updated CI 1

--- pr1/client.py
+++ pr1/client.py
@@ -1,6 +1,7 @@
 import xmlrpc.client
-proxy = xmlrpc.client.ServerProxy("http://localhost:8000/") 
-print("factorial of 3 is : %s" % str(proxy.factorial_rpc(3)))
-print("factorial of 5 is : %s" % str(proxy.factorial_rpc(5)))
+proxy = xmlrpc.client.ServerProxy("http://localhost:8000/") #A ServerProxy instance is an object that manages communication with a remote XML-RPC server
+n=int(input("Enter the number: "))
+print("factorial of given number is : %s" % str(proxy.factorial_rpc(n)))
+
 
 
--- pr1/server.py
+++ pr1/server.py
@@ -1,9 +1,16 @@
-from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCServer #XML-RPC is a Remote Procedure Call method that uses XML passed via HTTP(S) as a transport
 def factorial(n):
     fact = 1
     for i in range(1,n+1):
         fact=fact*i
     return fact
+
+#Remote Procedure Call (RPC) is a powerful technique for constructing distributed, client-server based applications.
+#RPC is kind of similar to REST (REpresentational State Transfer). In RPC, the method is found in the URL itself and the client submits calls that use arguments/parameters to use these methods.
+'''ADVANTAGES :
+
+1.RPC provides ABSTRACTION i.e message-passing nature of network communication is hidden from the user.
+2.With RPC code re-writing / re-developing effort is minimized. '''
 server = SimpleXMLRPCServer(("localhost", 8000), logRequests=True)
 server.register_function(factorial, "factorial_rpc")
 try:
